413596f400bf5248b9f04bf4a8efe1babdc1d5c8
Gabriel Oliveira <gabriels3t@hotmail.com>
2021-01-09 22:07:13 -0300

    Novo jeito de usar o format

--- PythonExercicios/ex015.py
+++ PythonExercicios/ex015.py
@@ -1,6 +1,6 @@
-dias_alugados = int(input('Quantos dias o carro foi alugado? '))
-km_rodados = float(input('Quantos Km foram rodados? '))
+  dias_alugados = int(input('Quantos dias o carro foi alugado? '))
+  km_rodados = float(input('Quantos Km foram rodados? '))
 
-total_a_pagar = (dias_alugados * 60) + (km_rodados * 0.15)
+  total_a_pagar = (dias_alugados * 60) + (km_rodados * 0.15)
 
-print('O total a pagar é de R${:.2f}'.format(total_a_pagar))
+  print(f'O total a pagar é de R${total_a_pagar :.2f}')
